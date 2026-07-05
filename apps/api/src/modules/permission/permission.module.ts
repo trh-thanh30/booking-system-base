41c78caa998b955110221a40ac5700cca0d0bfca
trh-thanh30 <tranhuuthanhcp@gmail.com>
2026-07-05 22:38:15 +0700

    feat(permission): reorder imports and add PrismaService to providers

--- apps/api/src/modules/permission/permission.module.ts
+++ apps/api/src/modules/permission/permission.module.ts
@@ -1,9 +1,10 @@
+import { PrismaService } from '@/database/prisma/prisma.service';
 import { PermissionController } from '@/modules/permission/permission.controller';
 import { PermissionRepository } from '@/modules/permission/repository/permission.repository';
 import { AssignUserPermissionsUseCase } from '@/modules/permission/use-cases/assign-user-permissions.use-case';
 import { GetUserPermissionsUseCase } from '@/modules/permission/use-cases/get-user-permissions.use-case';
-import { ListUserPermissionsUseCase } from '@/modules/permission/use-cases/list-user-permissions.use-case';
 import { ListPermissionsUseCase } from '@/modules/permission/use-cases/list-permissions.use-case';
+import { ListUserPermissionsUseCase } from '@/modules/permission/use-cases/list-user-permissions.use-case';
 import { ReplaceUserPermissionsUseCase } from '@/modules/permission/use-cases/replace-user-permissions.use-case';
 import { RevokeUserPermissionsUseCase } from '@/modules/permission/use-cases/revoke-user-permissions.use-case';
 import { Module } from '@nestjs/common';
@@ -11,6 +12,7 @@ import { Module } from '@nestjs/common';
 @Module({
   controllers: [PermissionController],
   providers: [
+    PrismaService,
     PermissionRepository,
     ListPermissionsUseCase,
     GetUserPermissionsUseCase,
